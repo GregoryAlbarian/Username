347352269914c26064a364899e940a5e94e1aecf
Gregory Albarian <albarian@chapman.edu>
2018-11-07 11:26:50 -0800

    working username.sh

--- username.sh
+++ username.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-# username.sh
+# username.sh by Gregory Albarian
  
 echo "Enter a username: "
 read USERNAME
 
-while echo $USERNAME | egrep -v "^[a-z][a-z_0-9]{3,12}$" > /dev/null 2>&1
+while echo $USERNAME | egrep -v "^[a-z][a-z_0-9]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - it must start with a lower case letter, only put lowercase letters, digits,"
 	echo "or underscore characters, and must contain at least three characters but no more than twelve!"
